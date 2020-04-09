f27a129fe5423f5d5dd9d01b08b0a6665d8fdb5f
ubongndoh <ubongndoh@gmail.com>
2020-04-10 00:34:34 +0100

    reversed

--- src/estimator.js
+++ src/estimator.js
@@ -26,7 +26,7 @@ const covid19ImpactEstimator = (data) => {
   const servercasesForICUByRequestedTime = Math.floor((5 / 100) * serverinfectionsByRequestedTime);
   // cases that will require ventilators
   const casesForVentilatorsByRequestedTime = Math.floor((2 / 100) * infectionsByRequestedTime);
-  const serCasesForVentByRequestedTime = Math.floor((2 / 100) * serverinfectionsByRequestedTime);
+  const servCasesForVentByRequestedTime = Math.floor((2 / 100) * serverinfectionsByRequestedTime);
   // amount of money to be lost in the economy
   const totalIncomePerperson = data.region.avgDailyIncomeInUSD * estimateTime;
   const dailyAvgIncome = data.region.avgDailyIncomePopulation;
@@ -52,7 +52,7 @@ const covid19ImpactEstimator = (data) => {
       severeCasesByRequestedTime: servlSevereCasesByRequestedTime,
       hospitalBedsByRequestedTime: serverehospitalBedsByRequestedTime,
       casesForICUByRequestedTime: servercasesForICUByRequestedTime,
-      casesForVentilatorsByRequestedTime: serCasesForVentByRequestedTime,
+      casesForVentilatorsByRequestedTime: servCasesForVentByRequestedTime,
       dollarsInFlight: svrDlrsInFlight
     }
   };
