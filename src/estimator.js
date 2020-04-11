bdb20c5aa68d33aeda1d9b5c4140076171b44102
ubongndoh <ubongndoh@gmail.com>
2020-04-11 23:14:11 +0100

    fixing 5e

--- src/estimator.js
+++ src/estimator.js
@@ -32,7 +32,7 @@ const covid19ImpactEstimator = (data) => {
   const dailyAvgIncome = data.region.avgDailyIncomePopulation;
   const dollarsInFlight = Math.trunc(infectionsByRequestedTime
     * dailyAvgIncome) / totalIncomePerperson;
-  const svrDlrsInFlight = Math.trunc(serverinfectionsByRequestedTime
+  const svrDlrsInFlight = (serverinfectionsByRequestedTime
     * dailyAvgIncome) / totalIncomePerperson;
   // return reponse data
   return {
